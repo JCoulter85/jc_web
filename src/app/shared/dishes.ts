0d4905a1b9a7bf7525eb8d82ca4b0a6e7d452462
James Coulter <jimcoulter85@gmail.com>
2019-10-16 20:44:49 -0700

    visual updates

--- src/app/shared/dishes.ts
+++ src/app/shared/dishes.ts
@@ -167,6 +167,47 @@ export const DISHES: Dish[] = [
                 date: '2011-12-02T17:57:28.556094Z'
             }
         ]
+    },
+    {
+        id: '3',
+        name: '4th Example',
+        image: '/assets/images/thumb-icon.png',
+        category: 'dessert',
+        featured: false,
+        label: '',
+        description: 'I will have to place a 4th work example within this card. Think of something and it will work awesomely...',
+        comments: [
+            {
+                rating: 5,
+                comment: 'Imagine all the eatables, living in conFusion!',
+                author: 'John Lemon',
+                date: '2012-10-16T17:57:28.556094Z'
+            },
+            {
+                rating: 4,
+                comment: 'Sends anyone to heaven, I wish I could get my mother-in-law to eat it!',
+                author: 'Paul McVites',
+                date: '2014-09-05T17:57:28.556094Z'
+            },
+            {
+                rating: 3,
+                comment: 'Eat it, just eat it!',
+                author: 'Michael Jaikishan',
+                date: '2015-02-13T17:57:28.556094Z'
+            },
+            {
+                rating: 4,
+                comment: 'Ultimate, Reaching for the stars!',
+                author: 'Ringo Starry',
+                date: '2013-12-02T17:57:28.556094Z'
+            },
+            {
+                rating: 2,
+                comment: 'It\'s your birthday, we\'re gonna party!',
+                author: '25 Cent',
+                date: '2011-12-02T17:57:28.556094Z'
+            }
+        ]
     }
 ];
 
