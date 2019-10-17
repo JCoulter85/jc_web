fbdd0cbb3f2bffa07acec32f45ef8bddf6fdf13e
James Coulter <jimcoulter85@gmail.com>
2019-10-17 14:01:23 -0700

    fake comment update

--- src/app/shared/dishes.ts
+++ src/app/shared/dishes.ts
@@ -1,217 +1,262 @@
-import { Dish } from './dish';
+import { Dish } from "./dish";
 
 export const DISHES: Dish[] = [
-
-
-    {
-        id: '0',
-        name: 'Angular Application',
-        image: '/assets/images/uthappizza.png',
-        category: 'Web Application',
-        featured: true,
-        label: 'Single Page App',
-        // tslint:disable-next-line:max-line-length
-        description: 'This web application was designed for a moch restaurant. I chose to building the foundation with the Angular framework for performance and design purposes. There are several features which include:  interactive menu, a rating system, a detailed contact page, jqury server, and much more!',
-        comments: [
-            {
-                rating: 5,
-                comment: 'Imagine all the eatables, living in conFusion!',
-                author: 'John Lemon',
-                date: '2012-10-16T17:57:28.556094Z'
-            },
-            {
-                rating: 4,
-                comment: 'Sends anyone to heaven, I wish I could get my mother-in-law to eat it!',
-                author: 'Paul McVites',
-                date: '2014-09-05T17:57:28.556094Z'
-            },
-            {
-                rating: 3,
-                comment: 'Eat it, just eat it!',
-                author: 'Michael Jaikishan',
-                date: '2015-02-13T17:57:28.556094Z'
-            },
-            {
-                rating: 4,
-                comment: 'Ultimate, Reaching for the stars!',
-                author: 'Ringo Starry',
-                date: '2013-12-02T17:57:28.556094Z'
-            },
-            {
-                rating: 2,
-                comment: 'It\'s your birthday, we\'re gonna party!',
-                author: '25 Cent',
-                date: '2011-12-02T17:57:28.556094Z'
-            },
-        ]
-    },
-    {
-        id: '1',
-        name: 'Raspberry Pi ULV',
-        image: '/assets/images/ulv-icon.jpg',
-        category: 'Personal Project',
-        featured: false,
-        label: '',
-        description: 'A fun project that allows the user to control their very own tank! Features include but not limited too: variable motor contorl, live IR video feed, capturing images with automated email to user, PlayStation 3 contoller input...',
-        comments: [
-            {
-                rating: 5,
-                comment: 'Imagine all the eatables, living in conFusion!',
-                author: 'John Lemon',
-                date: '2012-10-16T17:57:28.556094Z'
-            },
-            {
-                rating: 4,
-                comment: 'Sends anyone to heaven, I wish I could get my mother-in-law to eat it!',
-                author: 'Paul McVites',
-                date: '2014-09-05T17:57:28.556094Z'
-            },
-            {
-                rating: 3,
-                comment: 'Eat it, just eat it!',
-                author: 'Michael Jaikishan',
-                date: '2015-02-13T17:57:28.556094Z'
-            },
-            {
-                rating: 4,
-                comment: 'Ultimate, Reaching for the stars!',
-                author: 'Ringo Starry',
-                date: '2013-12-02T17:57:28.556094Z'
-            },
-            {
-                rating: 2,
-                comment: 'It\'s your birthday, we\'re gonna party!',
-                author: '25 Cent',
-                date: '2011-12-02T17:57:28.556094Z'
-            }
-        ]
-    },
-    {
-        id: '2',
-        name: 'Speed Up America Contributions',
-        image: '/assets/images/sua-icon.png',
-        category: 'Contributions',
-        featured: false,
-        label: 'New',
-        description: 'SpeedUpAmerica aims to provide on the ground truth about the Internet in America. I contribute to this program in hopes to help give the community some truth behind their internet speeds.',
-        comments: [
-            {
-                rating: 5,
-                comment: 'Imagine all the eatables, living in conFusion!',
-                author: 'John Lemon',
-                date: '2012-10-16T17:57:28.556094Z'
-            },
-            {
-                rating: 4,
-                comment: 'Sends anyone to heaven, I wish I could get my mother-in-law to eat it!',
-                author: 'Paul McVites',
-                date: '2014-09-05T17:57:28.556094Z'
-            },
-            {
-                rating: 3,
-                comment: 'Eat it, just eat it!',
-                author: 'Michael Jaikishan',
-                date: '2015-02-13T17:57:28.556094Z'
-            },
-            {
-                rating: 4,
-                comment: 'Ultimate, Reaching for the stars!',
-                author: 'Ringo Starry',
-                date: '2013-12-02T17:57:28.556094Z'
-            },
-            {
-                rating: 2,
-                comment: 'It\'s your birthday, we\'re gonna party!',
-                author: '25 Cent',
-                date: '2011-12-02T17:57:28.556094Z'
-            }
-        ]
-    },
-    {
-        id: '3',
-        name: '4th Example',
-        image: '/assets/images/thumb-icon.png',
-        category: 'dessert',
-        featured: false,
-        label: '',
-        description: 'I will have to place a 4th work example within this card. Think of something and it will work awesomely...',
-        comments: [
-            {
-                rating: 5,
-                comment: 'Imagine all the eatables, living in conFusion!',
-                author: 'John Lemon',
-                date: '2012-10-16T17:57:28.556094Z'
-            },
-            {
-                rating: 4,
-                comment: 'Sends anyone to heaven, I wish I could get my mother-in-law to eat it!',
-                author: 'Paul McVites',
-                date: '2014-09-05T17:57:28.556094Z'
-            },
-            {
-                rating: 3,
-                comment: 'Eat it, just eat it!',
-                author: 'Michael Jaikishan',
-                date: '2015-02-13T17:57:28.556094Z'
-            },
-            {
-                rating: 4,
-                comment: 'Ultimate, Reaching for the stars!',
-                author: 'Ringo Starry',
-                date: '2013-12-02T17:57:28.556094Z'
-            },
-            {
-                rating: 2,
-                comment: 'It\'s your birthday, we\'re gonna party!',
-                author: '25 Cent',
-                date: '2011-12-02T17:57:28.556094Z'
-            }
-        ]
-    },
-    {
-        id: '3',
-        name: '4th Example',
-        image: '/assets/images/thumb-icon.png',
-        category: 'dessert',
-        featured: false,
-        label: '',
-        description: 'I will have to place a 4th work example within this card. Think of something and it will work awesomely...',
-        comments: [
-            {
-                rating: 5,
-                comment: 'Imagine all the eatables, living in conFusion!',
-                author: 'John Lemon',
-                date: '2012-10-16T17:57:28.556094Z'
-            },
-            {
-                rating: 4,
-                comment: 'Sends anyone to heaven, I wish I could get my mother-in-law to eat it!',
-                author: 'Paul McVites',
-                date: '2014-09-05T17:57:28.556094Z'
-            },
-            {
-                rating: 3,
-                comment: 'Eat it, just eat it!',
-                author: 'Michael Jaikishan',
-                date: '2015-02-13T17:57:28.556094Z'
-            },
-            {
-                rating: 4,
-                comment: 'Ultimate, Reaching for the stars!',
-                author: 'Ringo Starry',
-                date: '2013-12-02T17:57:28.556094Z'
-            },
-            {
-                rating: 2,
-                comment: 'It\'s your birthday, we\'re gonna party!',
-                author: '25 Cent',
-                date: '2011-12-02T17:57:28.556094Z'
-            }
-        ]
-    }
+  {
+    id: "0",
+    name: "Angular Application",
+    image: "/assets/images/uthappizza.png",
+    category: "Web Application",
+    featured: true,
+    label: "Single Page App",
+    // tslint:disable-next-line:max-line-length
+    description:
+      "This web application was designed for a moch restaurant. I chose to building the foundation with the Angular framework for performance and design purposes. There are several features which include:  interactive menu, a rating system, a detailed contact page, jqury server, and much more!",
+    comments: [
+      {
+        rating: 5,
+        comment: "Wow! Good job on completing the class.",
+        author: "Sean Connery",
+        date: "2018-10-16T17:57:28.556094Z"
+      },
+      {
+        rating: 4,
+        comment: "Its pretty neat that you can post on here!",
+        author: "Matt Damon",
+        date: "2018-09-05T17:57:28.556094Z"
+      },
+      {
+        rating: 3,
+        comment: "You did this?!",
+        author: "George Clooney",
+        date: "2018-02-13T17:57:28.556094Z"
+      },
+      {
+        rating: 4,
+        comment: "Awesome work on changing career paths!",
+        author: "Edward Norton",
+        date: "2018-12-02T17:57:28.556094Z"
+      },
+      {
+        rating: 4,
+        comment: "Whoa!",
+        author: "Bill & Ted",
+        date: "2018-12-02T17:57:28.556094Z"
+      }
+    ]
+  },
+  {
+    id: "1",
+    name: "Raspberry Pi ULV",
+    image: "/assets/images/ulv-icon.jpg",
+    category: "Personal Project",
+    featured: false,
+    label: "",
+    description:
+      "A fun project that allows the user to control their very own tank! Features include but not limited too: variable motor contorl, live IR video feed, capturing images with automated email to user, PlayStation 3 contoller input...",
+    comments: [
+      {
+        rating: 5,
+        comment: "Imagine all the possibilities, living in the future!",
+        author: "Jack Johnson",
+        date: "2019-01-16T17:57:28.556094Z"
+      },
+      {
+        rating: 4,
+        comment: "WHAT?! That is seriously a cool project.",
+        author: "Paul Blart",
+        date: "2019-02-05T17:57:28.556094Z"
+      },
+      {
+        rating: 5,
+        comment: "Who doesn't want a personal Unmanned Land Vehicle",
+        author: "Michael Jackson",
+        date: "2019-03-13T17:57:28.556094Z"
+      },
+      {
+        rating: 4,
+        comment: "LEEROY JENKINS!",
+        author: "Rango Jackson",
+        date: "2019-04-02T17:57:28.556094Z"
+      },
+      {
+        rating: 5,
+        comment: "5 stars",
+        author: "MoJimbo Jones",
+        date: "2019-05-02T17:57:28.556094Z"
+      }
+    ]
+  },
+  {
+    id: "2",
+    name: "Speed Up America Contributions",
+    image: "/assets/images/sua-icon.png",
+    category: "Contributions",
+    featured: false,
+    label: "New",
+    description:
+      "SpeedUpAmerica aims to provide on the ground truth about the Internet in America. I contribute to this program in hopes to help give the community some truth behind their internet speeds.",
+    comments: [
+      {
+        rating: 5,
+        comment: "Good for you! Helping the community is pretty awesome.",
+        author: "John Goodman",
+        date: "2019-01-16T17:57:28.556094Z"
+      },
+      {
+        rating: 4,
+        comment: "Building your skills I see.",
+        author: "Brad Pitt",
+        date: "2019-02-05T17:57:28.556094Z"
+      },
+      {
+        rating: 3,
+        comment: "I always knew I wasn't getting what I paid for!",
+        author: "Robert De Niro",
+        date: "2019-03-13T17:57:28.556094Z"
+      },
+      {
+        rating: 4,
+        comment: "And just like that, I called my ISP.",
+        author: "Tom Hanks",
+        date: "2019-04-02T17:57:28.556094Z"
+      },
+      {
+        rating: 2,
+        comment: "People helping people. Good for you!",
+        author: "Bob Ross",
+        date: "2019-05-02T17:57:28.556094Z"
+      }
+    ]
+  },
+  {
+    id: "3",
+    name: "4th Example",
+    image: "/assets/images/thumb-icon.png",
+    category: "dessert",
+    featured: false,
+    label: "",
+    description:
+      "I will have to place a 4th work example within this card. Think of something and it will work awesomely...",
+    comments: [
+      {
+        rating: 5,
+        comment: "I've got a fever and this example is the only cure.",
+        author: "Christopher Walken",
+        date: "2019-01-16T17:57:28.556094Z"
+      },
+      {
+        rating: 4,
+        comment: "There is no spoon.",
+        author: "Keanu Reeves",
+        date: "2019-02-05T17:57:28.556094Z"
+      },
+      {
+        rating: 3,
+        comment: "I will find you...",
+        author: "Liam Neeson",
+        date: "2019-03-13T17:57:28.556094Z"
+      },
+      {
+        rating: 4,
+        comment: "It's all in the hips",
+        author: "Adam Sandler",
+        date: "2019-04-02T17:57:28.556094Z"
+      },
+      {
+        rating: 4,
+        comment: "Alright alright alright.",
+        author: "Matthew McConaughey",
+        date: "2019-05-02T17:57:28.556094Z"
+      }
+    ]
+  },
+  {
+    id: "4",
+    name: "5th Example",
+    image: "/assets/images/thumb-icon.png",
+    category: "dessert",
+    featured: false,
+    label: "",
+    description:
+      "I will have to place a 4th work example within this card. Think of something and it will work awesomely...",
+    comments: [
+      {
+        rating: 5,
+        comment: "Wow!",
+        author: "Owen Wilson",
+        date: "2019-01-16T17:57:28.556094Z"
+      },
+      {
+        rating: 4,
+        comment: "Its pretty neat that you can post on here!",
+        author: "Ben Stiller",
+        date: "2019-02-05T17:57:28.556094Z"
+      },
+      {
+        rating: 3,
+        comment: "You did this?!",
+        author: "Denzel Washington",
+        date: "2019-03-13T17:57:28.556094Z"
+      },
+      {
+        rating: 4,
+        comment: "Awesome work on changing career paths!",
+        author: "Johnny Depp",
+        date: "2019-04-02T17:57:28.556094Z"
+      },
+      {
+        rating: 4,
+        comment: "Whoa!",
+        author: "Ted",
+        date: "2019-05-02T17:57:28.556094Z"
+      }
+    ]
+  },
+  {
+    id: "5",
+    name: "6th Example",
+    image: "images/thumb-icon.png",
+    category: "",
+    featured: false,
+    label: "",
+    description:
+      "I will have to place a 5th work example within this card. Think of something and it will work awesomely...",
+    comments: [
+      {
+        rating: 5,
+        comment: "Wow!",
+        author: "Owen Wilson",
+        date: "2019-01-16T17:57:28.556094Z"
+      },
+      {
+        rating: 4,
+        comment: "Its pretty neat that you can post on here!",
+        author: "Ben Stiller",
+        date: "2019-02-05T17:57:28.556094Z"
+      },
+      {
+        rating: 3,
+        comment: "You did this?!",
+        author: "Denzel Washington",
+        date: "2019-03-13T17:57:28.556094Z"
+      },
+      {
+        rating: 4,
+        comment: "Awesome work on changing career paths!",
+        author: "Johnny Depp",
+        date: "2019-04-02T17:57:28.556094Z"
+      },
+      {
+        rating: 4,
+        comment: "Whoa!",
+        author: "Ted",
+        date: "2019-05-02T17:57:28.556094Z"
+      }
+    ]
+  }
 ];
 
 export class Feedback {
-    firstName: string = ('');
-    comment: string = ('');
-};
+  firstName: string = "";
+  comment: string = "";
+}
